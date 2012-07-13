e85a0fb16222629297e5fc05c1f114660cc2dde4
daydreamsandphobias <daydreamsandphobias@home.com>
2012-07-13 21:44:33 +0200

    Remove dependency from old clojure-contrib for the project.

--- project.clj
+++ project.clj
@@ -3,7 +3,6 @@
 
                Provides support for mining websites and newsfeeds."
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
                  [xerces/xercesImpl "2.9.1"]
                  [org.ccil.cowan.tagsoup/tagsoup "1.2"]
                  [xalan "2.7.1"]
